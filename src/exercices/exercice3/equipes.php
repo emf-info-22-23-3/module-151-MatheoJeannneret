7bf4fa80a9c729c3407a6a52db958ccb34b72a8d
MatheoJeannneret <matheo.jeanneret@gmail.com>
2025-01-27 13:27:55 +0100

    fin de l'exercice 3

--- src/exercices/exercice3/equipes.php
+++ src/exercices/exercice3/equipes.php
@@ -1,21 +1,31 @@
 <!doctype html>
 <html>
-  <header>
-    <link rel="stylesheet" type="text/css" href="stylesheets/main.css" />
+<header>
+  <link rel="stylesheet" type="text/css" href="stylesheets/main.css" />
 </header>
-  <body>
-    <div id="conteneur">
-      <h1>Les équipes de National League</h1>    
-      <table border= "1">
+
+<body>
+  <div id="conteneur">
+    <h1>Les équipes de National League</h1>
+    <table border="1">
       <tr>
         <td>ID</td>
         <td>Club</td>
       </tr>
       <?php
-        require('ctrl.php');
-        // A compléter....
+      function ajouteCelluleHtml($id, $equipe)
+      {
+        echo '<tr><td>' . $id . '</td><td>' . $equipe . '</td></tr>';
+      }
+      require('ctrl.php');
+      $equipes = getEquipes();
+      $id = 1;
+      foreach ($equipes as $equipe) {
+        ajouteCelluleHtml($id++, $equipe);
+      }
       ?>
-      </table>
-    </div>
-  </body>
+    </table>
+  </div>
+</body>
+
 </html>
